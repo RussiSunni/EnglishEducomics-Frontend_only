0c1d153b463cabeaf9405d074880a0ce3f1b78a1
Jonathan Dyason <jonathandyason@hotmail.com>
2018-09-24 16:28:04 +0200

    stuff

---
+++ scripts/sunshine.js
@@ -0,0 +1,25 @@
+
+function WriteCookie()
+{
+    cookievalue=  document.getElementById("username").value;
+    document.cookie= cookievalue;
+}
+
+$( '#usernameButton' ).click(function() {
+    WriteCookie();
+})
+
+var cookieArray = document.cookie.split(";");
+
+var username = cookieArray[0];
+
+
+window.onload = function() {
+  
+    $( '#username' ).html(username);
+  };
+
+
+
+
+
