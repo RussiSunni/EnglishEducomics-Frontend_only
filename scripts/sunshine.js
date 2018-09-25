ced9a08d20fbf0b7bc9fc5ed8163f26c6eee982e
Jonathan Dyason <jonathandyason@hotmail.com>
2018-09-25 20:59:50 +0200

    stuff

--- scripts/sunshine.js
+++ scripts/sunshine.js
@@ -1,4 +1,6 @@
 
+//cookies
+
 function WriteCookie()
 {
     cookievalue=  document.getElementById("username").value;
@@ -19,6 +21,45 @@ window.onload = function() {
     $( '#username' ).html(username);
   };
 
+
+  //modals
+
+var imgCount = document.getElementsByClassName("img-thumbnail").length;
+
+imgCount = imgCount + 1;
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+for (var x = 1; x < imgCount; x++) {
+    var img = document.getElementById("myImg" + x);
+
+var modalImg = document.getElementById("img01");
+
+var captionText = document.getElementById("caption");
+
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+       
+    }
+
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
 
 
